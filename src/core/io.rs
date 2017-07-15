e067468d0dc098ff650de29d2c200dbf16c95c68
James <jselby@jselby.net>
2017-07-15 12:55:36 +1000

    Implement data structure for sound registers

--- src/core/io.rs
+++ src/core/io.rs
@@ -10,14 +10,34 @@ use core::gpu::GPUMode;
 /// Storage for various I/O registers.
 pub struct IORegisters {
     pub p1 : u8,    // 0x00 - Joypad info and controller (R/W)
+    pub sb : u8,    // 0x01 - Serial transfer data (R/W)
     pub div : u16,  // 0x04 - Divider register (R/W)
     pub tima : u8,  // 0x05 - Timer Counter (R/W)
     pub tma : u8,   // 0x06 - Timer Modulo (R/W)
     pub tac : u8,   // 0x07 - Timer Control (R/W)
     pub iflag : u8, // 0x0F - (if) Interrupt Flag (R/W)
+    pub nr10 : u8,  // 0x10 - Channel 1 Sweep register (R/W)
+    pub nr11 : u8,  // 0x11 - Channel 1 Sound length/Wave pattern duty (R/W)
+    pub nr12 : u8,  // 0x12 - Channel 1 Volume Envelope (R/W)
+    pub nr13 : u8,  // 0x13 - Channel 1 Frequency lo (Write Only)
+    pub nr14 : u8,  // 0x14 - Channel 1 Frequency hi (R/W)
     pub nr21 : u8,  // 0x16 - Channel 2 Sound Length/Wave Pattern Duty (R/W)
+    pub nr22 : u8,  // 0x17 - Channel 2 Volume Envelope (R/W)
+    pub nr23 : u8,  // 0x18 - Channel 2 Frequency lo data (W)
+    pub nr24 : u8,  // 0x19 - Channel 2 Frequency hi data (R/W)
+    pub nr30 : u8,  // 0x1A - Channel 3 Sound on/off (R/W)
+    pub nr31 : u8,  // 0x1B - Channel 3 Sound Length
+    pub nr32 : u8,  // 0x1C - Channel 3 Select output level (R/W)
+    pub nr33 : u8,  // 0x1D - Channel 3 Frequency's lower data (W)
+    pub nr34 : u8,  // 0x1E - Channel 3 Frequency's higher data (R/W)
+    pub nr41 : u8,  // 0x20 - Channel 4 Sound Length (R/W)
+    pub nr42 : u8,  // 0x21 - Channel 4 Volume Envelope (R/W)
+    pub nr43 : u8,  // 0x22 - Channel 4 Polynomial Counter (R/W)
+    pub nr44 : u8,  // 0x23 - Channel 4 Counter/consecutive; Initial (R/W)
     pub nr50 : u8,  // 0x24 - Channel control / ON-OFF / Volume (R/W)
     pub nr51 : u8,  // 0x25 - Selection of Sound output terminal (R/W)
+    pub nr52 : u8,  // 0x26 - Sound on/off (R/W)
+    pub wave : [u8; 0x10], // Wave Pattern RAM
     pub dma : u8,   // 0x46 - DMA Transfer and Start Address (W)
 }
 
@@ -26,13 +46,33 @@ impl IORegisters {
         // TODO: Validate these
         return IORegisters {
             p1 : 0,
+            sb : 0,
             div : 0xABCC,
             tima : 0,
             tma : 0,
             tac : 0xF8,
-            nr21 : 0,
-            nr50 : 0,
-            nr51 : 0,
+            nr10 : 0x80,
+            nr11 : 0xBF,
+            nr12 : 0xF3,
+            nr13 : 0,
+            nr14 : 0xBF,
+            nr21 : 0x3F,
+            nr22 : 0x00,
+            nr23 : 0,
+            nr24 : 0xBF,
+            nr30 : 0x7F,
+            nr31 : 0xFF,
+            nr32 : 0x9F,
+            nr33 : 0,
+            nr34 : 0,
+            nr41 : 0xFF,
+            nr42 : 0x00,
+            nr43 : 0x00,
+            nr44 : 0,
+            nr50 : 0x77,
+            nr51 : 0xF3,
+            nr52 : 0xF1,
+            wave : [0; 0x10],
             iflag : 0,
             dma : 0
         }
@@ -64,14 +104,34 @@ pub fn read(mem : &GBMemory, ptr : u8) -> u8 {
 
             output
         }
+        0x02 => mem.ioregs.sb,
         0x04 => (mem.ioregs.div >> 8) as u8,
         0x05 => mem.ioregs.tima,
         0x06 => mem.ioregs.tma,
         0x07 => mem.ioregs.tac,
         0x0F => mem.ioregs.iflag | !(0b11111),
+        0x10 => mem.ioregs.nr10,
+        0x11 => mem.ioregs.nr11,
+        0x12 => mem.ioregs.nr12,
+        0x13 => 0xFF, // Write only
+        0x14 => mem.ioregs.nr14,
         0x16 => mem.ioregs.nr21,
+        0x17 => mem.ioregs.nr22,
+        0x18 => 0xFF, // Write only
+        0x19 => mem.ioregs.nr24,
+        0x1A => mem.ioregs.nr30,
+        0x1B => mem.ioregs.nr31,
+        0x1C => mem.ioregs.nr32,
+        0x1D => 0xFF, // Write only
+        0x1E => mem.ioregs.nr34,
+        0x20 => mem.ioregs.nr41,
+        0x21 => mem.ioregs.nr42,
+        0x22 => mem.ioregs.nr43,
+        0x23 => mem.ioregs.nr44,
         0x24 => mem.ioregs.nr50,
         0x25 => mem.ioregs.nr51,
+        0x26 => mem.ioregs.nr52,
+        0x30 ... 0x3F => mem.ioregs.wave[(ptr - 0x30) as usize],
         0x40 => mem.gpu.lcdc,
         0x41 => {
             if mem.gpu.lcdc >> 7 & 0x1 == 0 {
@@ -99,13 +159,12 @@ pub fn read(mem : &GBMemory, ptr : u8) -> u8 {
         0x49 => mem.gpu.obp1,
         0x4A => mem.gpu.wy,
         0x4B => mem.gpu.wx,
-        0x40 ... 0xFF => {
-            //println!("Unknown I/O register: {:02x}", ptr);
+        0x4C ... 0xFF => {
+            println!("WARN: Out of range I/O register: {:02x}", ptr);
             0xFF
         },
         _ => {
-            //println!("Unknown I/O register: {:02x}", ptr);
-            0xFF
+            panic!("Unknown I/O register: {:02x}", ptr);
         }
     }
 }
@@ -118,6 +177,7 @@ pub fn write(mem : &mut GBMemory, ptr : u8, val : u8) {
             // Serial
             //print!("{}", char::from_u32(val as u32).unwrap());
         },
+        0x02 => mem.ioregs.sb = val,
         0x04 => mem.ioregs.div = 0,
         0x05 => mem.ioregs.tima = val,
         0x06 => mem.ioregs.tma = val,
@@ -126,9 +186,28 @@ pub fn write(mem : &mut GBMemory, ptr : u8, val : u8) {
             mem.ioregs.iflag = val;
             mem.dirty_interrupts = true;
         },
+        0x10 => mem.ioregs.nr10 = val,
+        0x11 => mem.ioregs.nr11 = val,
+        0x12 => mem.ioregs.nr12 = val,
+        0x13 => mem.ioregs.nr13 = val,
+        0x14 => mem.ioregs.nr14 = val,
         0x16 => mem.ioregs.nr21 = val,
+        0x17 => mem.ioregs.nr22 = val,
+        0x18 => mem.ioregs.nr23 = val,
+        0x19 => mem.ioregs.nr24 = val,
+        0x1A => mem.ioregs.nr30 = val,
+        0x1B => mem.ioregs.nr31 = val,
+        0x1C => mem.ioregs.nr32 = val,
+        0x1D => mem.ioregs.nr33 = val,
+        0x1E => mem.ioregs.nr34 = val,
+        0x20 => mem.ioregs.nr41 = val,
+        0x21 => mem.ioregs.nr42 = val,
+        0x22 => mem.ioregs.nr43 = val,
+        0x23 => mem.ioregs.nr44 = val,
         0x24 => mem.ioregs.nr50 = val,
         0x25 => mem.ioregs.nr51 = val,
+        0x26 => mem.ioregs.nr52 = val,
+        0x30 ... 0x3F => mem.ioregs.wave[(ptr - 0x30) as usize] = val,
         0x40 => {
             let old_bit = mem.gpu.lcdc >> 7;
             let changed_bit = val >> 7;
@@ -168,11 +247,11 @@ pub fn write(mem : &mut GBMemory, ptr : u8, val : u8) {
         0x49 => mem.gpu.obp1 = val,
         0x4A => mem.gpu.wy = val,
         0x4B => mem.gpu.wx = val,
-        0x40 ... 0xFF => {
-            //println!("Unknown I/O register: {:02x} = {:02x}", ptr, val);
-        }
+        0x4C ... 0xFF => {
+            println!("WARN: Out of range I/O register: {:02x} = {:02x}", ptr, val);
+        },
         _ => {
-            //println!("Unknown I/O register: {:02x} = {:02x}", ptr, val);
+            panic!("Unknown I/O register: {:02x} = {:02x}", ptr, val);
         }
     }
 }
